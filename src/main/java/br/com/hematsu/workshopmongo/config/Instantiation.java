b2121f35c8906deb6c14902d9bb4d8ae4e2cd91c
Matheus Wendell Dantas Bezerra <matheuswdb1703@gmail.com>
2025-11-29 16:36:41 -0300

    Criando entity Post com User aninhado

--- src/main/java/br/com/hematsu/workshopmongo/config/Instantiation.java
+++ src/main/java/br/com/hematsu/workshopmongo/config/Instantiation.java
@@ -1,32 +1,46 @@
 package br.com.hematsu.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.hematsu.workshopmongo.domain.Post;
 import br.com.hematsu.workshopmongo.domain.User;
+import br.com.hematsu.workshopmongo.repository.PostRepository;
 import br.com.hematsu.workshopmongo.repository.UserRepository;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
     private UserRepository userRepository;
+    private PostRepository postRepository;
 
-    Instantiation(UserRepository userRepository) {
+    Instantiation(UserRepository userRepository, PostRepository postRepository) {
         this.userRepository = userRepository;
+        this.postRepository = postRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 
 }
---
+++ src/main/java/br/com/hematsu/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package br.com.hematsu.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import br.com.hematsu.workshopmongo.domain.Post;
+
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
