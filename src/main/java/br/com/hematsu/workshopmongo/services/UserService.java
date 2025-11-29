04f3e2d0c7c57547329c14109ff6e281fe2281a9
Matheus Wendell Dantas Bezerra <matheuswdb1703@gmail.com>
2025-11-29 15:00:39 -0300

    Obtendo um usuário por id

--- src/main/java/br/com/hematsu/workshopmongo/services/UserService.java
+++ src/main/java/br/com/hematsu/workshopmongo/services/UserService.java
@@ -1,11 +1,13 @@
 package br.com.hematsu.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import br.com.hematsu.workshopmongo.domain.User;
 import br.com.hematsu.workshopmongo.repository.UserRepository;
+import br.com.hematsu.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,5 +20,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
